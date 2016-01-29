be10ee89517d226a4bded903cd1fc27101e5d7d0
taivop <taivo.pungas@gmail.com>
2016-01-29 17:43:06 +0100

    adding stuff before cleaning up

--- build_hdrs.m
+++ build_hdrs.m
@@ -1,19 +1,6 @@
-%
-%      This HDR Toolbox demo creates an HDR radiance map:
-%	   1) Read a stack of LDR images
-%	   2) Read exposure values from the EXIF
-%	   3) Estimate the Camera Response Function (CRF)
-%	   4) Build the radiance map using the stack and stack_exposure
-%	   5) Save the radiance map in .hdr format
-%	   6) Show the tone mapped version of the radiance map
-%
-%       Author: Francesco Banterle
-%       Copyright 2015 (c)
-%
-
-%clear all;
 
 dirs = dir('images');
+disp('START BUILDING HDR IMAGES');
 
 for directory=dirs'
     %directory.name
@@ -22,13 +9,15 @@ for directory=dirs'
     fprintf('is "%s" dir? %d\n', dir_path, isdir(dir_path))
     
     if isdir(dir_path) && ~strcmp(dir_name, '.') && ~strcmp(dir_name, '..')
+        fprintf('----- Processing directory "%s" -----\n', dir_name);
         
         format = 'jpg';
-        
-        fprintf('----- Processing directory "%s" -----\n', dir_name);
 
         disp('1) Read a stack of LDR images');
         stack = ReadLDRStack(dir_path, format);
+        
+        disp('1.5) Align images');
+        stack = SiftAlignment(stack, 1);
 
         disp('2) Read exposure values from the exif');
         stack_exposure = ReadLDRStackInfo(dir_path, format);
@@ -41,8 +30,8 @@ for directory=dirs'
 
         disp('4) Build the radiance map using the stack and stack_exposure');
         imgHDR = BuildHDR(stack, stack_exposure, 'LUT', lin_fun, 'Deb97', 'log');
-
-        disp('5) Save the radiance map in the .hdr format');
+        
+        disp('6) Save the radiance map in the .hdr format');
         hdrimwrite(imgHDR, sprintf('%s/hdr_image.pfm', dir_path));
 
         %disp('6) Show the tone mapped version of the radiance map with gamma encoding');
@@ -51,3 +40,5 @@ for directory=dirs'
         %GammaTMO(ReinhardBilTMO(imgHDR, 0.18), 2.2, 0, 1);
     end;
 end;
+
+disp('DONE BUILDING HDR IMAGES');
